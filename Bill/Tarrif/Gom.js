1e67bcde1379b0b5394444255a24dc6fcc06dc50
mrhb <mmmhajjar83gmail.com>
2021-01-05 18:26:46 +0330

    bill complete

--- Bill/Tarrif/Gom.js
+++ Bill/Tarrif/Gom.js
@@ -42,9 +42,22 @@ days=end.diff(start,'day');
 
 console.log(
     PieceCost(param.x,param.y,kwh[0],kwh[1],kwh[2],kwh[3])
-);
 
-    return [energy,avg_energy,pf,zian,demandC,DemandFactor,demandCost];
+);
+    energyCost=kwh[0]+kwh[1]+kwh[2];
+    varrCost=zian*(demandCost+energyCost);
+    faslCost=0.2*(varrCost+demandCost+energyCost)/(29*days);
+    totalCost=faslCost+varrCost+demandCost+energyCost;
+    kwhCost=0;
+    if(energyCost=0)
+    kwhCost=totalCost/energyCost;
+    kwhlowNorm=kwh[0]/(days*12);
+    kwhmeanNorm=kwh[1]/(days*12);
+    kwhhighNorm=kwh[2]/(days*12);
+    
+
+    return [energy,avg_energy,pf,zian,demandC,DemandFactor,
+        demandCost,energyCost,varrCost,faslCost,totalCost,kwhCost];
 }
     
 
