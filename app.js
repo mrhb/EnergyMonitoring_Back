77a6315419e8f78a2228afece054f478e4100e4c
Mohammad Javad Imani <mjimani.ir@gmail.com>
2021-01-13 19:14:48 +0330

    last change

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('./middleware/cors');
 
 const app = express();
 
@@ -12,12 +13,9 @@ const app = express();
 // Routes
 require('./route/user/user.route')(app);
 
-// Cors
-require('./middleware/cors')(app);
-
-
 // app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors);
 app.use(bodyParser.json());
 
 module.exports = app;
