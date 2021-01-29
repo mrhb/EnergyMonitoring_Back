a0426a618ad7ab06a15b09797800c81797d88f33
Mohammad Javad Imani <mjimani.ir@gmail.com>
2021-01-29 18:00:41 +0330

    last changes

--- controller/sharing/powerSharing.controller.js
+++ controller/sharing/powerSharing.controller.js
@@ -218,7 +218,7 @@ exports.getListPageableByFilter = async (req, res, next) => {
     }
 
     let powerSharingListCount = await powerSharingDao
-        .getListPageableByFilterCount(filter)
+        .getListPageableByFilterCount()
         .then(result => {
             return result;
         }).catch(err => console.log(err));
