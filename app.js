4bb00bf3b689fe7b614a539f4c03354dc76da546
mrhb <mmmhajjar83gmail.com>
2021-01-30 17:31:03 +0330

    first mr.Imani works publish

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('./middleware/cors');
 const app = express();
+const Root=__dirname+'\\ui';
+
 
 // app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -13,10 +15,18 @@ app.use(cors);
 
 app.use(bodyParser.json());
 
+app.use('/ui',express.static(Root));
 
 // Routes
+
+
 require('./route/user/user.route')(app);
 require('./route/file/file.route')(app);
 require('./route/region/region.route')(app);
+
+app.get('/ui/*', (req,res) => {
+    res.sendFile(Root+"/index.html")
+  });
+
 
 module.exports = app;
