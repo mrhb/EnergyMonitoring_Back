f857515388d0eb9af23a96910448b0c81dc33679
Mohammad Javad Imani <mjimani.ir@gmail.com>
2021-02-15 19:30:02 +0330

    create validation for power receipt

--- controller/receipt/dto/reqCreatePowerReceipt.dto.js
+++ controller/receipt/dto/reqCreatePowerReceipt.dto.js
@@ -6,39 +6,41 @@ module.exports = ReqCreatePowerReceipt;
 
 function ReqCreatePowerReceipt(data, userId, powerSharing, next) {
     validate(data, next);
-    // if (data.name !== null && data.name !== 'undefined') {
-    //     this.name = data.name;
-    // }
-    // if (this.address !== null && this.address !== 'undefined') {
-    //     this.address = data.address;
-    // }
-    // this.billingId = data.billingId;
-    // if (this.systemPass !== null && this.systemPass !== 'undefined') {
-    //     this.systemPass = data.systemPass;
-    // }
-    // if (this.city !== null && this.city !== 'undefined') {
-    //     this.city = data.city;
-    // }
-    // if (this.domainCode !== null && this.domainCode !== 'undefined') {
-    //     this.domainCode = data.domainCode;
-    // }
-    // this.addressCode = data.addressCode;
-    // if (this.numberShare !== null && this.numberShare !== 'undefined') {
-    //     this.numberShare = data.numberShare;
-    // }
-    // if (this.fileNumber !== null && this.fileNumber !== 'undefined') {
-    //     this.fileNumber = data.fileNumber;
-    // }
-    // if (this.serialShare !== null && this.serialShare !== 'undefined') {
-    //     this.serialShare = data.serialShare;
-    // }
-    // this.useType = data.useType;
-    // this.useCode = data.useCode;
-    // this.group = data.group;
-    // this.capacity = data.capacity;
-    // this.coefficient = data.coefficient;
-    // this.voltageType = data.voltageType;
-    // this.powerSupplyVoltage = data.powerSupplyVoltage;
+
+    this.paymentCode = data.paymentCode;
+    this.period = data.period;
+    this.fromDate = new Date(data.fromDate);
+    this.toDate = new Date(data.toDate);
+    const getNumberDays = (this.toDate.getTime() - this.fromDate.getTime()) / (1000 * 3600 * 24);
+    this.numberDays = getNumberDays;
+    this.explanationExpenses = data.explanationExpenses;
+    this.previousCounter = data.previousCounter;
+    this.currentCounter = data.currentCounter;
+    this.coefficient = data.coefficient;
+    this.totalConsumption = data.totalConsumption;
+    this.totalConsumptionLastChanges = data.totalConsumptionLastChanges;
+    this.rate = data.rate;
+    this.amount = data.amount;
+    this.intermediate = data.intermediate;
+    this.peakLoad = data.peakLoad;
+    this.lowLoad = data.lowLoad;
+    this.peakTimesFriday = data.peakTimesFriday;
+    this.reactive = data.reactive;
+    this.contractualPower = data.contractualPower;
+    this.calculatedPower = data.calculatedPower;
+    this.maximeterNumber = data.maximeterNumber;
+    this.powerConsumption = data.powerConsumption;
+    this.badConsumptionLossRatio = data.badConsumptionLossRatio;
+    this.paymentDeadLine = data.paymentDeadLine;
+    this.consumptionAmount = data.consumptionAmount;
+    this.subscription = data.subscription;
+    this.powerPrice = data.powerPrice;
+    this.seasonPrice = data.seasonPrice;
+    this.badPenaltiesForConsumingElectricityDuringThePeriod = data.badPenaltiesForConsumingElectricityDuringThePeriod;
+    this.vat = data.vat;
+    this.electricalTolls = data.electricalTolls;
+    this.debt = data.debt;
+    this.payableAmount = data.payableAmount;
 
     this.powerSharingId = powerSharing.id;
     this.numberShare = powerSharing.numberShare;
@@ -60,52 +62,97 @@ function validate(data, next) {
         data.period !== 'ELEVENTH' && data.period !== 'TWELFTH') {
         throw next("دوره درست انتخاب نشده است.");
     }
-    // if (!data.useCode) {
-    //     throw next("کد تعرفه نمیتواند خالی باشد.");
-    // }
-    // if (data.period === 'HOME') {
-    //     if (data.useCode !== 'NORMAL_REGION_NON_WARM_TROPICAL' && data.useCode !== 'WARM_TROPICAL_4' && data.useCode !== 'WARM_TROPICAL_3' && data.useCode !== 'WARM_TROPICAL_2' && data.useCode !== 'WARM_TROPICAL_1') {
-    //         throw next("کد تعرفه درست انتخاب نشده است.");
-    //     }
-    // } else if (data.period === 'GENERAL') {
-    //     if (data.useCode !== 'TWO_A_1' && data.useCode !== 'TWO_A_2' && data.useCode !== 'TWO_B') {
-    //         throw next("کد تعرفه درست انتخاب نشده است.");
-    //     }
-    // } else if (data.useType === 'WATER_PRODUCTS') {
-    //     if (data.useCode !== 'THREE_A' && data.useCode !== 'THREE_B' && data.useCode !== 'THREE_J_1' && data.useCode !== 'THREE_J_2') {
-    //         throw next("کد تعرفه درست انتخاب نشده است.");
-    //     }
-    // } else if (data.useType === 'INDUSTRY_PRODUCTS') {
-    //     if (data.useCode !== 'FOUR_A_1' && data.useCode !== 'FOUR_A_2' && data.useCode !== 'FOUR_A_3' && data.useCode !== 'FOUR_B_1' && data.useCode !== 'FOUR_B_2' && data.useCode !== 'FOUR_B_3') {
-    //         throw next("کد تعرفه درست انتخاب نشده است.");
-    //     }
-    // } else if (data.useType === 'OTHER') {
-    //     if (data.useCode !== 'MORE_THAN_30_KW' && data.useCode !== 'LESS_THAN_30_KW_NON_WARM' && data.useCode !== 'LESS_THAN_30_KW_WARM') {
-    //         throw next("کد تعرفه درست انتخاب نشده است.");
-    //     }
-    // }
-    // if (!data.group) {
-    //     throw next("گروه نمیتواند خالی باشد.");
-    // }
-    // if (data.group !== 'DIMANDI' && data.group !== 'UN_DIMANDI' ) {
-    //     throw next("گروه درست انتخاب نشده است.");
-    // }
-    // if (!data.capacity) {
-    //     throw next("ظرفیت نمیتواند خالی باشد.");
-    // }
-    // if (!data.coefficient) {
-    //     throw next("ضریب اشتراک نمیتواند خالی باشد.");
-    // }
-    // if (!data.voltageType) {
-    //     throw next("نوع ولتاژ نمیتواند خالی باشد.");
-    // }
-    // if (data.voltageType !== 'PRIMITIVE' && data.voltageType !== 'SECONDARY' ) {
-    //     throw next("نوع ولتاژ درست انتخاب نشده است.");
-    // }
-    // if (!data.powerSupplyVoltage) {
-    //     throw next("ولتاژ تغذیه نمیتواند خالی باشد.");
-    // }
-    // if (data.powerSupplyVoltage !== 'P380' && data.powerSupplyVoltage !== 'P220' ) {
-    //     throw next("ولتاژ تغذیه درست انتخاب نشده است.");
-    // }
+    if (!data.fromDate) {
+        throw next("از تاریخ نمیتواند خالی باشد.");
+    }
+    if (!data.toDate) {
+        throw next("تا تاریخ نمیتواند خالی باشد.");
+    }
+    if (new Date(data.fromDate).getTime() > new Date(data.toDate).getTime()) {
+        throw next("تاریخ شروع نمیتواند بعد از تاریخ پایان باشد.");
+    }
+    if (!data.explanationExpenses) {
+        throw next("شرح مصارف نمیتواند خالی باشد.");
+    }
+    if (!data.previousCounter) {
+        throw next("شمارنده قبلی نمیتواند خالی باشد.");
+    }
+    if (!data.currentCounter) {
+        throw next("شمارنده کنونی نمیتواند خالی باشد.");
+    }
+    if (!data.coefficient) {
+        throw next("ضریب نمیتواند خالی باشد.");
+    }
+    if (!data.totalConsumption) {
+        throw next("مصرف کل نمیتواند خالی باشد.");
+    }
+    if (!data.totalConsumptionLastChanges) {
+        throw next("مصرف بعد از آخرین تغییرات نمیتواند خالی باشد.");
+    }
+    if (!data.rate) {
+        throw next("نرخ نمیتواند خالی باشد.");
+    }
+    if (!data.amount) {
+        throw next("مبلغ نمیتواند خالی باشد.");
+    }
+    if (!data.intermediate) {
+        throw next("میان باری نمیتواند خالی باشد.");
+    }
+    if (!data.peakLoad) {
+        throw next("اوج بار نمیتواند خالی باشد.");
+    }
+    if (!data.lowLoad) {
+        throw next("کم بار نمیتواند خالی باشد.");
+    }
+    if (!data.peakTimesFriday) {
+        throw next("اوج بار جمعه نمیتواند خالی باشد.");
+    }
+    if (!data.reactive) {
+        throw next("راکتیو نمیتواند خالی باشد.");
+    }
+    if (!data.contractualPower) {
+        throw next("قدرت قراردادی نمیتواند خالی باشد.");
+    }
+    if (!data.calculatedPower) {
+        throw next("قدرت محاسبه شده نمیتواند خالی باشد.");
+    }
+    if (!data.maximeterNumber) {
+        throw next("عدد ماکسیمتر نمیتواند خالی باشد.");
+    }
+    if (!data.powerConsumption) {
+        throw next("قدرت مصرفی نمیتواند خالی باشد.");
+    }
+    if (!data.badConsumptionLossRatio) {
+        throw next("ضریب زیان بدی مصرف نمیتواند خالی باشد.");
+    }
+    if (!data.paymentDeadLine) {
+        throw next("مهلت پرداخت نمیتواند خالی باشد.");
+    }
+    if (!data.consumptionAmount) {
+        throw next("مبلغ مصرف نمیتواند خالی باشد.");
+    }
+    if (!data.subscription) {
+        throw next("آبونمان نمیتواند خالی باشد.");
+    }
+    if (!data.powerPrice) {
+        throw next("بهای قدرت نمیتواند خالی باشد.");
+    }
+    if (!data.seasonPrice) {
+        throw next("بهای فصل نمیتواند خالی باشد.");
+    }
+    if (!data.badPenaltiesForConsumingElectricityDuringThePeriod) {
+        throw next("جریمه بدی مصرف بهای برق دوره نمیتواند خالی باشد.");
+    }
+    if (!data.vat) {
+        throw next("مالیات بر ارزش افزوده نمیتواند خالی باشد.");
+    }
+    if (!data.electricalTolls) {
+        throw next("عوارض برق نمیتواند خالی باشد.");
+    }
+    if (!data.debt) {
+        throw next("بدهکاری کسر هزار ریال نمیتواند خالی باشد.");
+    }
+    if (!data.payableAmount) {
+        throw next("مبلغ قابل پرداخت نمیتواند خالی باشد.");
+    }
 }
