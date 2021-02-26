2a87d331d30f045f74d8ed964f191e2e563629aa
mrhb <mmmhajjar83gmail.com>
2021-02-26 12:08:34 +0330

    ConsumptionSchema

--- model/receipt/powerReceipt.model.js
+++ model/receipt/powerReceipt.model.js
@@ -7,6 +7,16 @@ const Schema = mongoose.Schema;
 
 const powerSharingAllocation = require('./powerSharingAllocation.model');
 
+const ConsumptionSchema = new Schema({
+    preCounter          :  {type: String, required: true},//شمارنده قبلی
+    currentCounter      :  {type: String, required: true},//شمارنده کنونی
+    coefficient         :  {type: String, required: true},//ضریب
+    totalConsumption      :  {type: String, required: true},//مصرف کل
+    consumptionAfterLastChange      :  {type: String, required: true},//مصرف بعد از آخرین تغییرات
+    nerkh               :  {type: String, required: true},//نرخ
+    mablagh             :  {type: String, required: true}//مبلغ
+});
+
 const PowerReceiptSchema = new Schema({
 
     powerSharingId: {type: String}, // شناسه شاشتراک برق
@@ -44,11 +54,13 @@ const PowerReceiptSchema = new Schema({
     totalConsumptionLastChanges: {type: String, required: true}, // مصرف بعد از آخرین تغییرات
     rate: {type: String, required: true}, // نرخ
     amount: {type: String, required: true}, // مبلغ
-    intermediate: {type: String, required: true}, // میان باری
-    peakLoad: {type: String, required: true}, // اوج بار
-    lowLoad: {type: String, required: true}, // کم بار
-    peakTimesFriday: {type: String, required: true}, // اوج بار جمعه
-    reactive: {type: String, required: true}, // راکتیو
+    //*******Consumptions******* */
+    intermediate: {type: ConsumptionSchema, required: true}, // میان باری
+    peakLoad: {type: ConsumptionSchema, required: true}, // اوج بار
+    lowLoad: {type: ConsumptionSchema, required: true}, // کم بار
+    peakTimesFriday: {type: ConsumptionSchema, required: true}, // اوج بار جمعه
+    reactive: {type: ConsumptionSchema, required: true}, // راکتیو
+    //*************** */
     contractualPower: {type: String, required: true}, // قدرت قراردادی
     calculatedPower: {type: String, required: true}, // قدرت محاسبه شده
     maximeterNumber: {type: String, required: true}, // عدد ماکسیمتر
@@ -59,7 +71,8 @@ const PowerReceiptSchema = new Schema({
     subscription: {type: String, required: true}, // آبونمان
     powerPrice: {type: Number, required: true}, // بهای قدرت
     seasonPrice: {type: Number, required: true}, // بهای فصل
-    badPenaltiesForConsumingElectricityDuringThePeriod: {type: Number, required: true}, // جریمه بدی مصرف بهای برق دوره
+    badPenaltiesForConsuming: {type: Number, required: true}, // جریمه بدی مصرف 
+    electricityDuringThePeriod: {type: Number, required: true}, // بهای برق دوره
     vat: {type: Number, required: true}, // مالیات بر ارزش افزوده
     electricalTolls: {type: Number, required: true}, // عوارض برق
     debt: {type: Number, required: true}, // بدهکاری کسر هزار ریال
@@ -71,6 +84,8 @@ const PowerReceiptSchema = new Schema({
     timestamps: true
 });
 
+
+
 PowerReceiptSchema.set('toJSON', {
     virtuals: true,
     versionKey: false,
@@ -80,3 +95,86 @@ PowerReceiptSchema.set('toJSON', {
 });
 
 module.exports = mongoose.model('powerReceipt', PowerReceiptSchema);
+
+
+/**
+ * نمونه داده قبض برق
+ */
+
+// {
+//     "_id" : ObjectId("60312ebc47908dae37cb28c3"),
+//     "powerSharingId" : "60372d4d7356f03a2cb0efb2",
+//     "paymentCode" : "2535256",
+//     "period" : "FIRST",
+//     "fromDate" : "2020-01-01",
+//     "toDate" : "2020-01-30",
+//     "numberDays" : 12,
+//     "explanationExpenses" : "ssss",
+//     "previousCounter" : 2535,
+//     "currentCounter" : 3535,
+//     "coefficient" : "2155sdsd",
+//     "totalConsumption" : 25353256,
+//     "totalConsumptionLastChanges" : 3.23213213,
+//     "rate" : "25",
+//     "amount" : 250000,
+//     "intermediate" : {
+//         "preCounter" : 324,
+//         "currentCounter" : 23453,
+//         "coefficient" : 877,
+//         "totalConsumption" : 986,
+//         "consumptionAfterLastChange" : 13245,
+//         "nerkh" : 5467,
+//         "mablagh" : 9865
+//     },
+//     "peakLoad" : {
+//         "preCounter" : 324,
+//         "currentCounter" : 23453,
+//         "coefficient" : 877,
+//         "totalConsumption" : 986,
+//         "consumptionAfterLastChange" : 13245,
+//         "nerkh" : 5467,
+//         "mablagh" : 9865
+//     },
+//     "lowLoad" : {
+//         "preCounter" : 324,
+//         "currentCounter" : 23453,
+//         "coefficient" : 877,
+//         "totalConsumption" : 986,
+//         "consumptionAfterLastChange" : 13245,
+//         "nerkh" : 5467,
+//         "mablagh" : 9865
+//     },
+//     "peakTimesFriday" : {
+//         "preCounter" : 324,
+//         "currentCounter" : 23453,
+//         "coefficient" : 877,
+//         "totalConsumption" : 986,
+//         "consumptionAfterLastChange" : 13245,
+//         "nerkh" : 5467,
+//         "mablagh" : 9865
+//     },
+//     "reactive" : {
+//         "preCounter" : 7856,
+//         "currentCounter" : 6345,
+//         "coefficient" : 971,
+//         "totalConsumption" : 13457,
+//         "consumptionAfterLastChange" : 13245,
+//         "nerkh" : 186764,
+//         "mablagh" : 9987865
+//     },
+//     "contractualPower" : "sdasdsad",
+//     "calculatedPower" : "sdasdsad",
+//     "maximeterNumber" : "sdasdsad",
+//     "powerConsumption" : "sdasdsad",
+//     "badConsumptionLossRatio" : "sdasdsad",
+//     "paymentDeadLine" : 250000,
+//     "consumptionAmount" : 250000,
+//     "subscription" : "sdasdsad",
+//     "powerPrice" : 2500,
+//     "seasonPrice" : 35000,
+//     "badPenaltiesForConsumingElectricityDuringThePeriod" : 2500000,
+//     "vat" : 2533333,
+//     "electricalTolls" : 352222,
+//     "debt" : 25555,
+//     "payableAmount" : 350000
+// }
