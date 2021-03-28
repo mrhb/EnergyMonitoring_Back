9a34c6b4d3f33d759fe1a8ee07cabdc447a449c9
mrhb <mmmhajjar83gmail.com>
2021-03-28 10:12:03 +0430

    تحلیل ظرفیت کنتور گاز

--- analysis/suscriptions/gas/gas.controller.js
+++ analysis/suscriptions/gas/gas.controller.js
@@ -1,14 +1,53 @@
 /**
  * @author M.Reza hajjar
  */
-
+const gasSharingDao = require('../../../dao/sharing/gasSharing.dao');
 const Response = require('../../../middleware/response/response-handler');
 
 
-exports.getOne = async (req, res, next) => {
-  //  console.log('user.id ' + req.user.id);
-    res.send(Response("sfdvergvrfd"));
+exports.capacity = async (req, res, next) => {
+//    console.log('user.id ' + req.user.id);
+    if (req.body.regionId){
+        this.regionId = req.body.regionId;
+    }else {
+        this.regionId = "";
+    }
 
 
+    let CapacityListByRegion = await gasSharingDao
+    .getCapacityListByRegion(this.regionId)
+    .then(result => {
+        return result;
+    }).catch(err => console.log(err));
+
+    let labels=[];
+    let capacity=CapacityListByRegion.reduce((acc,value)=>{
+        if(!acc[value.capacity])
+        {
+            acc[value.capacity]={data:[],name:value.capacity};
+        }
+
+        if(!labels[value.title])
+        {
+            labels.push(value.title);
+        }
+        acc[value.capacity].data.push(value.Count);
+         return acc
+    },{});
+
+
+    CapacityList=[];
+
+    for (const [key, value] of Object.entries(capacity)) {
+        CapacityList.push(value)
+      }
+
+
+      function onlyUnique(value, index, self) {
+        return self.indexOf(value) === index;
+      }
+      
+      // usage example:
+      labels = labels.filter(onlyUnique);
+    res.send(Response({"series":CapacityList,"labels":labels}));
 };
-
--- analysis/suscriptions/suscriptions.route.js
+++ analysis/suscriptions/suscriptions.route.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
 
     let gasController = require('./gas/gas.controller');
 
-    app.get(SUBSCRIPTIONS + 'get-one', gasController.getOne);
+    app.get(SUBSCRIPTIONS + 'capacity', gasController.capacity);
     
     
     // app.post(config.API + 'auth/login', authController.login);
