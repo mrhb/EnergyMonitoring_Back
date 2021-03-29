2cc2ce05506b7be03fb69e5d22b24621bd3d1601
mrhb <mmmhajjar83gmail.com>
2021-03-29 12:36:30 +0430

    مشکل تعداد در تحلی کنتور های گاز حل شد

--- analysis/suscriptions/gas/gasAnalysis.controller.js
+++ analysis/suscriptions/gas/gasAnalysis.controller.js
@@ -20,36 +20,26 @@ exports.capacity = async (req, res, next) => {
         return result;
     }).catch(err => console.log(err));
 
+    let series=[];
     let labels=[];
-    let capacity=CapacityListByRegion.reduce((acc,value)=>{
+    CapacityListByRegion.reduce((acc,value)=>{
         if(!acc[value.capacity])
         {
             acc[value.capacity]={data:[],name:value.capacity};
+            series.push({data:[],name:value.capacity});
         }
-
-        if(!labels[value.title])
+    
+        if(!labels.find(lbl=>lbl==value.title))
         {
             labels.push(value.title);
         }
+    
+       var seri= series.find(element=>element.name==value.capacity)
+       seri.data[labels.indexOf(value.title)]=value.Count;
         acc[value.capacity].data.push(value.Count);
          return acc
     },{});
-
-
-    CapacityList=[];
-
-    for (const [key, value] of Object.entries(capacity)) {
-        CapacityList.push(value)
-      }
-
-
-      function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
-      }
-      
-      // usage example:
-      labels = labels.filter(onlyUnique);
-    res.send(Response({"series":CapacityList,"labels":labels}));
+    res.send(Response({"series":series,"labels":labels}));
 };
 exports.consuption = async (req, res, next) => {
     //    console.log('user.id ' + req.user.id);
