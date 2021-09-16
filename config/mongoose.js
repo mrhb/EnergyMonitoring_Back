c25f793fb1bf8cae88675feed1da2f5417b12991
mrhb <mmmhajjar83gmail.com>
2021-09-16 19:43:04 +0430

    server changed

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,8 +3,9 @@
  * phone : +989035074205
  */
 let mongoose = require('mongoose');
-const DbLink = 'mongodb://185.252.29.76:27017/';
-const DbName = 'EnergyMonitoring';
+// const DbLink = 'mongodb://185.252.29.76:27017/';
+const DbLink = 'mongodb://195.248.243.157:27017/';
+const DbName = 'MRHB_EnergyMonitoring';
 
 mongoose.connect( DbLink, {
     poolSize: 10,
